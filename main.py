93beb0f1ceabbd2269b07ccb55a6f1c1b2fd046e
zaher-abb <abboudz@th-brandenburg.de>
2021-04-22 13:53:22 +0200

    add translate function for twits

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
 import gzip
 import json
 import tweepy as tw
-import pandas as pd
+import pandas as df
 
 from google_trans_new import google_translator
 
-
-translator=google_translator()
+translator = google_translator()
 with gzip.open('', 'r') as zipfile:
     twitter_data = json.loads(zipfile.read().decode('ascii'))
 
 
-def twitter_search(searched_topic, list_of_twitts):
+# print( json.dumps(twitter_data[516], indent=4, sort_keys=True))
+
+
+def twitter_search(searched_topic):
     lead_users = []
-    users={}
-    for i in list_of_twitts:
+
+    for i in twitter_data:
         try:
             hashtags = []
             for hashtag in i['entities']['hashtags']:
                 hashtags.append(hashtag['text'])
 
             if searched_topic in i['text'] or searched_topic in hashtags:
-
-                '''translate the twits to English '''
-                # translated_twit
-                # if translator.detect(i['text'])[0] == 'en' or translator.detect(i['text'])[0] == 'de' :
-                #     translated_twit = translator.translate(i['text'], lang_tgt='en')
-
-                # list of Tuple (user name , number of followers , the text of the twit ,twit url , number of retweets
-
                 lead_users.append(
                     [i['user']['name'], i['user']['followers_count'], i['text'], i['user']['url'], i['retweet_count']])
 
@@ -39,25 +33,29 @@ def twitter_search(searched_topic, list_of_twitts):
     return lead_users
 
 
-temp_list = twitter_search('3d  ', twitter_data)
-pd.set_option('display.max_columns', 6)
-
-
-def get_lead_user(list_of_twitts):
-    # list_of_users.sort(key=lambda x: x[1], reverse=True)
-    # # sort the list with number of follower and get the first half of the list
-    # sorted_list_by_followers = [x for x in
-    #                             sorted(temp_list, key=lambda x: x[1], reverse=True)[:int(len(list_of_users) / 2)]]
-    # # sort the list with number of twits and get the first half of the list
-    # sorted_list_by_retweets = [x for x in sorted(sorted_list_by_followers, key=lambda x: x[4], reverse=True)[
-    #                                       :int(len(list_of_users) / 2)]]
-    tweet_text = pd.DataFrame(data=list_of_twitts,
+def get_lead_user(list_of_twits):
+    tweet_text = df.DataFrame(data=list_of_twits,
                               columns=['user', "followers_count", 'text', 'url', 'retweet_count'])
     sorted_list_by_followers = tweet_text.sort_values(by=['followers_count', 'retweet_count'], ascending=False)
 
-    """return the top ten element of the list"""""
     return sorted_list_by_followers.head(10)
 
 
-print((get_lead_user(temp_list)))
+def get_who_had_the_most_twits(list_of_twits):
+    return df['user'].value_counts().head(15)
 
+
+def translate_twits(list_of_twits):
+    for index, row in list_of_twits.iterrows():
+
+        if translator.detect(row['text'])[0] != 'en' or translator.detect(row['text'])[0] != 'de':
+            temp = translator.translate(row['text'], lang_tgt='de')
+            list_of_twits.at[index, 'text'] = temp
+
+    return list_of_twits
+
+
+df.set_option('display.max_columns', 6)
+df.set_option("display.max_colwidth", None)
+
+print(translate_twits(get_lead_user(twitter_search(input("what do you want to search ?\n")))))
